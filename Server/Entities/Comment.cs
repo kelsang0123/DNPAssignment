ab4ac4858301345fb565dd9f084027302b383503
Kelsang64 <sherpakelsang139@gmail.com>
2025-11-01 18:02:54 +0100

    intial repository commits

---
+++ Server/Entities/Comment.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Entities;
+
+public class Comment
+{
+      public int Id { get; set; }
+        public string Body { get; set; }
+        public int UserId { get; set; }
+        public int PostId { get; set; }
+}
---
+++ Server/Entities/Post.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Entities;
+
+public class Post
+{
+ public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int UserId { get; set; }
+}
