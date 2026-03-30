79e49c60df381d96f57b9c3fd99e19f1b3ff5925
kelsang <kelsangsp2005@gmail.com>
2026-03-30 22:21:58 +0200

    Initial entities commit

---
+++ Server/Entities/Comment.cs
@@ -0,0 +1,7 @@
+namespace Entities;
+
+public class Comment
+{
+public int Id{get; set;}
+public required string Body{get; set;}
+}
---
+++ Server/Entities/Post.cs
@@ -0,0 +1,8 @@
+namespace Entities;
+
+public class Post
+{
+public int Id{get; set;}
+public required string Title{get; set;}
+public required string Password{get; set;} 
+}
